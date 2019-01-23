c4bf78d960848459f9de210123f2ae50a5ab53ac
preecej <preecej@science.oregonstate.edu>
2019-01-23 14:00:44 -0800

    revised output msg

--- scripts/release/orthopairs/move_and_rename_orthology_files.py
+++ scripts/release/orthopairs/move_and_rename_orthology_files.py
@@ -87,8 +87,8 @@ def rename_and_deploy(dict_projected_species, ensembl_input_path, inparanoid_inp
             inparanoid_species_count += 1
             shutil.copy2(inparanoid_input_path + "/Oryza_sativa.japonica.IRGSP_" + genus + "_" + species.lower() + ".txt",
                       output_orthopair_path + "/slice_" + str(slice_version_number) + "/" + v[1] + "/inparanoid_os_2_" + v[1].lower() + "_sorted.tab")
-
-        print(k,v)
+        if args.verbose:
+            print(k,v)
 
     print("ensembl_species_count: " + str(ensembl_species_count) + ", inparanoid_species_count: " + str(inparanoid_species_count))
 
@@ -97,7 +97,12 @@ def rename_and_deploy(dict_projected_species, ensembl_input_path, inparanoid_inp
 # process args
 parser = argparse.ArgumentParser(
     description='Distribute orthology files from both Ensembl Compara and Inparanoid downloads. Move and rename, '
-                'based on config file of projected species abbvs.')
+                'based on config file of projected species abbvs. Sample execution:'
+                'move_and_rename_orthology_files.py -c ./Plant_Reactome_projection_list.tsv '
+                '-e ~/Documents/projects/plant_reactome/releases/r60/orthology_data/compara/v2 '
+                '-i ~/Documents/projects/plant_reactome/releases/r60/orthology_data/inparanoid '
+                '-o ~/Documents/projects/plant_reactome/plant_reactome_site/projection/rice_to '
+                '-s 17 -r 42 -v')
 
 # input settings
 parser.add_argument('-c', '--projected_species_configs', help='tab file containing projected species and abbvs', required=True)
@@ -107,11 +112,13 @@ parser.add_argument('-i', '--inparanoid_input_path', help='path to inparanoid su
 #  ~/Documents/projects/plant_reactome/releases/r60/orthology_data/inparanoid/test
 parser.add_argument('-s', '--slice_version_number', type=int)
 parser.add_argument('-r', '--ensembl_release_number', type=int)
-parser.add_argument('-v', '--verbose', action='store_true')
 
 # output settings
 parser.add_argument('-o', '--output_orthopair_path', help='path to renamed, redistributed orthology files, binned in folders by species abbv')
 #  ~/Documents/projects/plant_reactome/plant_reactome_site/projection/rice_to
+
+# other settings
+parser.add_argument('-v', '--verbose', action='store_true')
 
 args = parser.parse_args()
 if args.verbose:
@@ -120,5 +127,7 @@ if args.verbose:
 dict_projected_species = load_configs(args.projected_species_configs)
 rename_and_deploy(dict_projected_species, args.ensembl_input_path, args.inparanoid_input_path,
                   args.slice_version_number, args.ensembl_release_number, args.output_orthopair_path)
+
+print('Files renamed and moved.')
 
 # end ------------------------------------------------------------------------------------------------------------------
